683445d801930b924f6976cb410dc4449d1b453d
chandana M <chandu11288@gmail.com>
2018-05-30 23:59:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const currentUser = 'Chandana';
+const currentUser = 'Chandana M';
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
